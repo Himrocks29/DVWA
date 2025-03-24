4dcdcd686e895624830f0a18626584dd46a4df6a
Himanshu Sharma <himrocks@Himanshus-MacBook-Air.local>
2025-03-24 17:05:19 +0530

    hard_Coded

--- DVWA/vulnerabilities/sqli/test.php
+++ DVWA/vulnerabilities/sqli/test.php
@@ -1,7 +1,14 @@
 <?php
-$host = "192.168.0.7";
+/*$host = "192.168.0.7";
 $username = "dvwa";
-$password = "password";
+$password = "password";*/
+
+/*Fix Start*/
+require_once 'config.php';
+$config = require 'config.php';
+$username = $config['username'];
+$password = $config['password'];
+/*Fix END*/
 
 mssql_connect($host, $username, $password);
 mssql_select_db($database);
